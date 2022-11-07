c5ce5983baee246f184b647831c47a9abf572da5
nathan <nat2013@laposte.net>
2022-11-07 14:57:27 +0100

    correction namespace et ajout double authentification

--- src/classes/action/AddUserAction.php
+++ src/classes/action/AddUserAction.php
@@ -15,6 +15,7 @@ class AddUserAction extends Action {
             <form action="?action=${_GET['action']}" method="post">
                 <label>Email: </label><input type="text" name="email" placeholder="toto@gmail.com" required>
                 <label>Password: </label><input type="password" name="password" required>
+                <label>Password: (verification) </label><input type="password" name="password2" required>
                 <button type="submit">Validate</button>
             </form>
             HTML;
@@ -22,17 +23,25 @@ class AddUserAction extends Action {
 
             $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
             $passwd = filter_var($_POST['password']);
-            try {
-                if (Auth::register($email, $passwd)) $html = "succesfully registered";
-                else $html = "email already exist";
-                $html .= "<br><a href='?action=signin'>Connection</a>";
-            } catch (EmailAlreadyRegistedException $e) {
-                $html = "email has already registed";
-                $html .= "<br><a href='?action=add-user'>Retour</a>";
-            } catch (TooShortPasswordException $e) {
-                $html = "password too short";
-                $html .= "<br><a href='?action=add-user'>Retour</a>";
+            $passwd2 = filter_var($_POST['password2']);
+            if($passwd != $passwd2){
+                $html = "les 2 mdp ne sont pas identiques";
             }
+            else
+            {
+                try {
+                    if (Auth::register($email, $passwd)) $html = "succesfully registered";
+                    else $html = "email already exist";
+                    $html .= "<br><a href='?action=signin'>Connection</a>";
+                } catch (EmailAlreadyRegistedException $e) {
+                    $html = "email has already registed";
+                    $html .= "<br><a href='?action=add-user'>Retour</a>";
+                } catch (TooShortPasswordException $e) {
+                    $html = "password too short";
+                    $html .= "<br><a href='?action=add-user'>Retour</a>";
+                }
+            }
+
         }
         return $html;
     }
