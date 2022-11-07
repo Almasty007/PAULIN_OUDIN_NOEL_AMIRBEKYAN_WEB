a77aca2833f773bf38cc7f02756359ba01bd680a
oudin-clement <clementoudin3@gmail.com>
2022-11-07 17:14:05 +0100

    fix Sessionpref

--- src/classes/list/SessionPref.php
+++ src/classes/list/SessionPref.php
@@ -1,10 +1,17 @@
 <?php
 
 require_once "vendor/autoload.php";
+
 use iutnc\sae\classes\list\PrefList;
 
-session_start();
+class SessionPref
+{
+    function start(): void
+    {
+        session_start();
 
-if(!isset($_SESSION['prefList'])){
-    $_SESSION['prefList'] = new PrefList();
+        if (!isset($_SESSION['prefList'])) {
+            $_SESSION['prefList'] = new PrefList();
+        }
+    }
 }
