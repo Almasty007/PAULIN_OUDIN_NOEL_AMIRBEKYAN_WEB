8d9f1bc4ebb46174fc5bdaa2ee8dd5ab26e27574
Alexandre Noel <noel295u@etu.univ-lorraine.fr>
2022-11-08 15:28:24 +0100

    Ajout css

--- index.php
+++ index.php
@@ -9,12 +9,11 @@ use iutnc\sae\action\AddUserAction;
 
 session_start();
 ConnectionFactory::setConfig("DBConfig.ini");
-if (isset($_SESSION['user'])){
-    if (isset($_GET['action'])){
+if (isset($_SESSION['user'])) {
+    if (isset($_GET['action'])) {
         $dispatcher = new Dispatcher();
         $dispatcher->run();
-    }
-    else {
+    } else {
         $action = <<<HTML
             <p>HTML</p>
             <a href="?action=logout">Logout</a>
@@ -22,17 +21,29 @@ HTML;
         echo $action;
     }
 
-}
-else {
-    if (isset($_GET['action'])){
+} else {
+    if (isset($_GET['action'])) {
         $dispatcher = new Dispatcher();
         $dispatcher->run();
-    }
-    else {
+    } else {
         $action = <<<HTML
-            <a href="?action=add-user">inscription</a><br>
-            <a href="?action=signin">connection</a><br>
-        HTML;
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title>NetVOD</title>
+            <link rel="stylesheet" type="text/css" href="style.css">
+        </head>
+        <body>
+            <div class="header">
+                <a id="title" href="">NetVOD</a>
+                <div class="header-bottom">
+                    <a id="signin" href="?action=signin">Sign in</a>
+                    <a id="signup" href="?action=add-user">Sign up</a>
+                </div>
+            </div>
+        </body>
+    </html>
+HTML;
         echo $action;
     }
 }
