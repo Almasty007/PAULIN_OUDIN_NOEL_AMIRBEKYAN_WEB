b7ac53188780e8bc444afd0152e1051cdf427b39
Alexandre Noel <noel295u@etu.univ-lorraine.fr>
2022-11-09 15:43:15 +0100

    Fix

--- src/classes/action/ListePrefAction.php
+++ src/classes/action/ListePrefAction.php
@@ -10,14 +10,14 @@ class ListePrefAction extends Action
         $res = "";
         $bd = ConnectionFactory::makeConnection();
         $id = $_SESSION['id'];
-        $rep0 = $bd->query("select count(*)from listPref where idUtilisateur = '$id'");
+        $rep0 = $bd->query("select count(*)from listPref where idUser = '$id'");
         $r1 = $rep0->fetch();
         $nbrFilmPref = $r1[0];
         if($nbrFilmPref == 0){
             $res.= "<p>Pas de film préféré</p>";
         }
         else{
-            $rep = $bd->query("select serie.id, titre from listPref inner join serie on serie.id = listPref.idSerie where idUtilisateur = '$id'");
+            $rep = $bd->query("select serie.id, titre from listPref inner join serie on serie.id = listPref.idSerie where idUser = '$id'");
             while ($row = $rep->fetch()){
                 $res.="<a href=?action=serie&id=".$row[0].">".$row[1]."</a></br>";
             }
