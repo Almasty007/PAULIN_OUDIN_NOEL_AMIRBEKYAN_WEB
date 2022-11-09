d099e5955003695a5855458cdd3c2a0a3a010fab
alena <alenamirbekyan18@gmail.com>
2022-11-09 11:40:50 +0100

    affichage episode

--- src/classes/media/Episode.php
+++ src/classes/media/Episode.php
@@ -18,7 +18,7 @@ class Episode
         $row = $query->fetch();
         $res .= "<div class=\"description\"><p>Epidode numero : ".$row[1]."Titre : ".$row[2]."</p><p>Resume : ".$row[3]."</p><p>Duree : ".$row[4]."</div>";
 
-        $res.='<video controls width="400"><source src="/sae/video/'.$row[5].'"> type="video/webm"';
+        $res.='<video controls width="1000"><source src="/sae/video/'.$row[5].'"> type="video/webm"';
         $res.="<a href='?action=catalogue'>Retour</a>";
         return $res;
     }
