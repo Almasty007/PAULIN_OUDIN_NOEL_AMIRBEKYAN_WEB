2154c2629275fda70b515b9c5744b0ed9685c4bb
alena <alenamirbekyan18@gmail.com>
2022-11-09 14:57:11 +0100

    y a un bug

---
+++ src/classes/baseChange/AjouterEnCour.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace iutnc\sae\baseChange;
+
+use iutnc\sae\db\ConnectionFactory;
+
+class AjouterEnCour
+{
+
+    public static function execute()
+    {
+        $res = "";
+        $bd = ConnectionFactory::makeConnection();
+        $idUser = $_SESSION['id'];
+        $id_serie = $_GET["id"];
+        $req01 = $bd->query("select count(*) from EnCour where idserie = $id_serie and iduser = $idUser");
+        $r01 = $req01->fetch();
+        if ($r01 == 0) {
+            $req = $bd->prepare("insert into EnCour values(?,?)");
+            $req->bindParam(1, $id_serie);
+            $req->bindParam(2, $idUser);
+            $req->execute();
+        }
+    }
+}
